7b38341b5af853619ea5be2a7034f55a53d1c552
Alessandro Maroso <alessandro.maroso@iks.it>
2016-02-04 10:08:53 +0100

    Initial Commit

---
+++ EarthWallpapers/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  EarthWallpapers
+//
+//  Created by Alessandro Maroso on 04/02/16.
+//  Copyright © 2016 membersheep. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ EarthWallpapersTests/EarthWallpapersTests.swift
@@ -0,0 +1,36 @@
+//
+//  EarthWallpapersTests.swift
+//  EarthWallpapersTests
+//
+//  Created by Alessandro Maroso on 04/02/16.
+//  Copyright © 2016 membersheep. All rights reserved.
+//
+
+import XCTest
+@testable import EarthWallpapers
+
+class EarthWallpapersTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
